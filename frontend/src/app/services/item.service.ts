8d430b17e98e5211b12d93b626d0a9b8cc59b79e
suhaibsuhai <suhaibhamza300@gmail.com>
2024-11-21 19:13:02 +0000

    service changed

--- frontend/src/app/services/item.service.ts
+++ frontend/src/app/services/item.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Item } from '../models/item.model';
-import { environment } from '../environments/environment';  // Import environment
+import { environment } from '../environments/environment.prod';  // Import environment
 
 @Injectable({
   providedIn: 'root'
