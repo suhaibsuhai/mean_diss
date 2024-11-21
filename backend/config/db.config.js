f048bf18bcbce29b0f59aab1deb3afecee997b94
suhaibsuhai <suhaibhamza300@gmail.com>
2024-11-21 16:11:29 +0000

    backend

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,5 +1,5 @@
+require('dotenv').config();  // Load environment variables
+
 module.exports = {
-    url: 'mongodb://localhost:27017/meancrud',  // Local MongoDB URL
-    // For MongoDB Atlas or remote DB, use a URL like: 'mongodb+srv://username:password@cluster.mongodb.net/dbname'
-  };
-  
+    url: process.env.MONGO_URI,  // Use MongoDB URL from environment variables
+};
